4f3ec478bb294b9552dcd2ba62fa8d445a703b1a
Your Name <you@example.com>
2023-03-09 13:58:05 +0000

    Updated code

--- labs/continuous-integration/src/HelloWorld/Program.cs
+++ labs/continuous-integration/src/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ public class Program
         if (args.Length == 1)
         {
             int bedtimeHour = 22;
-            int.TryParse(args[0], out bedtimeHour);
+            int.TryParse(args[1], out bedtimeHour);
             var today = DateTime.Today;
             var bedtime = new DateTime(today.Year, today.Month, today.Day, bedtimeHour, 0, 0);
             var duration = bedtime.Subtract(DateTime.Now);
